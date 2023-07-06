2bd993f96826b4b97a6c22b054adea8a98fa0ed1
Angie-540 <janiceangella21@gmail.com>
2023-07-06 23:51:31 +0300

    interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
+import { ContactComponent } from './pages/contact/contact.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { MindfulnessComponent } from './pages/mindfulness/mindfulness.component';
+import { TherapyComponent } from './pages/therapy/therapy.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {
+    path: "Home",
+    component: HomeComponent
+  },
+  {
+    path: "Articles",
+    component: ArticlesComponent
+  },
+  {
+    path: "Mindfulness",
+    component: MindfulnessComponent
+  },
+  {
+    path: "Therapy",
+    component: TherapyComponent
+  },
+  {
+    path: "Contact",
+    component: ContactComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { MindfulnessComponent } from './pages/mindfulness/mindfulness.component';
+import { TherapyComponent } from './pages/therapy/therapy.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArticlesComponent,
+    MindfulnessComponent,
+    TherapyComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
